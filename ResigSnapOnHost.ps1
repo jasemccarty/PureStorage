7c4982b5f482185864d4d00dfb546ee6ab87de09
Jase McCarty <jasemccarty@users.noreply.github.com>
2021-02-16 15:25:23 -0700

    Resignature a presented Snapshot on a single ESXi
    This script will allow a vSphere Administrator to re-signature & present one or more snapshot volumes to an ESXi host.
    Must connect to the ESXi host with Connect-VIServer
    The VMFS datastore will be mounted as "snap-random-OLD_DATASTORE_NAME"
    
    Typical use case is for regaining quick access to a VMFS volume where vCenter was stored, 
    for times when a VCSA file system check does not help restore operation to vCenter Server.

---
+++ ResigSnapOnHost.ps1
@@ -0,0 +1,41 @@
+# Check to see if a current vCenter Server session is in place
+If ($Global:DefaultVIServer) {
+    Write-Host "Connected to " -NoNewline 
+    Write-Host $Global:DefaultVIServer -ForegroundColor Green
+    If ($Global:DefaultVIServer.ProductLine -ne "embeddedEsx") {
+        Write-Host "This is not an ESXi host. Please connect to an individual ESXi host"
+        return
+    }
+} else {
+    Write-Host "Not connected to vCenter Server" -ForegroundColor Red
+    $VIFQDN = Read-Host "Please enter the vCenter Server FQDN"  
+    $VICredentials = Get-Credential -Message "Enter credentials for vCenter Server" 
+    try {
+        Connect-VIServer -Server $VIFQDN -Credential $VICredentials -ErrorAction Stop | Out-Null
+        Write-Host "Connected to $VIFQDN" -ForegroundColor Green 
+        If ($Global:DefaultVIServer.ProductLine -ne "embeddedEsx") {
+            Write-Host "This is not an ESXi host. Please connect to an individual ESXi host"
+            return
+        } 
+    }
+    catch {
+        Write-Host "Failed to connect to $VIFQDN" -BackgroundColor Red
+        Write-Host $Error
+        Write-Host "Terminating the script " -BackgroundColor Red
+        return
+    }
+}
+
+
+$EsxCli = Get-EsxCli -VMHost (Get-VMhost) -V2
+
+$Snaps = $esxcli.storage.vmfs.snapshot.list.invoke()
+
+if ($Snaps.Count -gt 0) {
+    Foreach ($Snap in $Snaps) {
+        Write-Host "Snapshot Found: $($Snap.VolumeName)"
+        $esxcli.storage.vmfs.snapshot.resignature.invoke(@{volumelabel=$($Snap.VolumeName)})
+    }
+} else {
+    Write-Host "No Snapshot volumes found"
+}
